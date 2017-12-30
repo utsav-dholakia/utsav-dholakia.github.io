783ae6e7a74b2e80273c862b4c6fe447b882e48f
Utsav Dholakia <uvd1992@gmail.com>
2017-12-30 15:32:12 -0600

    Commit styles, js and static files

---
+++ js/customScript.js
@@ -0,0 +1,12 @@
+/*function scrollTo(param){
+	var targetDiv = param;
+	console.log(targetDiv);
+	console.log($(targetDiv).html());
+	$(window).scrollTo(document.getElementById(targetDiv));
+	$(window).location.hash = $('#'+param);
+}
+$(document).ready(function(){
+	$('.scrollButton').on('click',function(){
+		$.scrollTo('+=100px');
+	})
+});*/
